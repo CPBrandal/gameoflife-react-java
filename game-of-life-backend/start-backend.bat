28676def67134fb9a5c41a7229ec260375f59648
cpbranda <cpbranda@uio.no>
2024-09-17 16:53:00 +0200

    small fixes

--- game-of-life-backend/start-backend.bat
+++ game-of-life-backend/start-backend.bat
@@ -1,3 +1,3 @@
 @echo off
 cd /d "%~dp0"
-"C:\Program Files\Java\jdk-22\bin\java.exe" @C:\Users\phili\AppData\Local\Temp\cp_zfd815ku1nj6ruq9ae7q922r.argfile com.example.game_of_life_backend.GameOfLifeBackendApplication
+"C:\Program Files\Java\jdk-22\bin\java.exe" -cp build/libs/*.jar com.example.game_of_life_backend.GameOfLifeBackendApplication
